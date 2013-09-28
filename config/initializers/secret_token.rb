49286467ecda0996c907235b57522fba66f24172
Andy Lindeman <andy@andylindeman.com>
2013-09-27 22:06:06 -0400

    New Rails application

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Octotest::Application.config.secret_key_base = '6c83811f482e4cde83923bdff9485a40b3f514a6e6388a7e0ffa6329963f92eb8ce54601a51fdca61b5a2622417910fc0d3d4e425fa048eeaf2da8e2155f9d59'
